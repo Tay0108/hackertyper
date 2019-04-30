12ed90fe711dca8af482361f99895822371a85e1
Tay0108 <kjkosecki@gmail.com>
2019-04-30 23:08:09 +0200

    alpha version working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const code = `
+public class Solution {
+
+    public static int reverseNumber(int n) {
+        int reversed = 0;
+
+        while (n > 0) {
+            reversed = reversed * 10 + n % 10;
+            n = n / 10;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int n) {
+        return n == reverseNumber(n);
+    }
+
+    public static void main(String[] args) {
+
+        int max = 0;
+
+        for (int i = 999; i > 99; i--) {
+            for (int j = i; j > 99; j--) {
+                if (isPalindrome(i * j)) {
+                    if(max < i * j) {
+                        max = i * j;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
+}
+`;
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: "",
+      counter: 3,
+    };
+    this.addCharacter = this.addCharacter.bind(this);
+  }
+
+  addCharacter() {
+    console.log("add character");
+    this.setState(prevState => {
+      return {
+        code: code.substring(0, prevState.counter),
+        counter: prevState.counter + 3,
+      }});
+  }
+
+  render() {
+    return (
+        <div className="App" onKeyPress={this.addCharacter} tabIndex="0">
+          {this.state.code}
+        </div>
+    );
+  }
 }
 
 export default App;
